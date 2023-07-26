f48751a4cf2b69ed81dae72a5c64e602ff8b3c74
signkj <signkj@nate.com>
2023-07-26 23:47:57 +0900

    init3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,14 @@ export default function Home() {
     <>
       <main className={styles.main}>
         <div>
-          <h1>창우야 공부 욜심히 해라</h1>
+          <h1>창우야</h1>
+          <h1>공부 욜심히 해라</h1>
           <h1>시간 금방 간다.</h1>
-          <h1>&emsp; &emsp; 알갓쥐?</h1>
+          <h1>알갓쥐?</h1>
           <br />
           <br />
           <br />
-          <h2>
-            {' '}
-            &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; 기정이가
-          </h2>
+          <h2> &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; 기정이가</h2>
         </div>
       </main>
     </>
